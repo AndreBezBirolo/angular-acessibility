056a4a91196f9a1d64fd8654f57860c94c964962
André Bez Birolo <andrecentrobrasileiro@outlook.com>
2021-09-27 14:51:03 -0300

    Fechando o modal clicando no overlay

--- src/app/shared/components/modal/models/modal-ref.ts
+++ src/app/shared/components/modal/models/modal-ref.ts
@@ -6,7 +6,6 @@ export class ModalRef {
   constructor(private componentRef: ComponentRef<ModalComponent>) {}
 
   public close(): void {
-    console.log('close called');
     this.componentRef.destroy();
   }
 }
